29f59052e408d12ca17810f4bdb8f3fbf7a10a1f
Rehan Chanaka <rehang@99x.lk>
2020-09-20 13:25:39 +0530

    update deploy script

--- deploy-script.sh
+++ deploy-script.sh
@@ -2,10 +2,10 @@
 imageName=track-my-diet:1.0
 containerName=tmd
 
-docker build -t $imageName -f Dockerfile  .
+sudo docker build -t $imageName -f Dockerfile  .
 
 echo Delete old container...
-docker rm -f $containerName
+sudo docker rm -f $containerName
 
 echo Run new container...
-docker run -d -p 80:1024 --name $containerName $imageName
+sudo docker run -d -p 80:1024 --name $containerName $imageName
